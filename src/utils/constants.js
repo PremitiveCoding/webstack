aeb72511f1fa069d391bb2034c316272657d6265
PremitiveCoding <58826270+PremitiveCoding@users.noreply.github.com>
2022-09-08 15:35:49 +0200

    ok

---
+++ src/utils/constants.js
@@ -0,0 +1,24 @@
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Web Development', icon: <HomeIcon />, },
+  { name: 'MERN Stack', icon: <CodeIcon />, },
+  { name: 'MEAN Stack', icon: <CodeIcon />, },
+  { name: 'LAMP Stack', icon: <CodeIcon />, },
+  { name: 'MEAV Stack', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Laravel Inertia VueJS', icon: <CodeIcon />, },
+  { name: 'Django and Pyhthon', icon: <CodeIcon />, }
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
